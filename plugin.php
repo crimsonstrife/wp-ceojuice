1303fda5bdfeb2c4f84de4fa0278163f28261159
Patrick Barnhardt <contact@patrickbarnhardt.info>
2022-04-26 14:53:01 -0400

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -11,11 +11,13 @@ Text Domain:  ceojuice-api
 
  * @Author: crimsonstrife
  * @Date: 2022-04-26 14:49:01
- * @Last Modified by:   crimsonstrife
- * @Last Modified time: 2022-04-26 14:49:01
-*/
+ * @Last Modified by: crimsonstrife
+ * @Last Modified time: 2022-04-26 14:52:50
+ * @requires: PHP 8.0+
+ * @requires: WP 5.9.0+
+ * @requires: ID230 on the CeoJuice API
+ * @requires: PHPfastCache 9.0.0+
 
-/*
 Requires PHPfastCache (included in this plugin)
 Requires PHP 8.0+ because of the PHPfastCache library
 With caching disabled you may be able to run this plugin on lower versions of PHP, but this is untested.
