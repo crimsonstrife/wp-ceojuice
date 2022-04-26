5fd9628c8db1a2d4c8d62e6abdfe61f7a0e7b492
Patrick Barnhardt <contact@patrickbarnhardt.info>
2022-04-26 14:49:35 -0400

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -8,7 +8,15 @@ Author:       Patrick Barnhardt
 Author URI:   https://www.patrickbarnhardt.com
 License:      GPL3
 Text Domain:  ceojuice-api
+
+ * @Author: crimsonstrife
+ * @Date: 2022-04-26 14:49:01
+ * @Last Modified by:   crimsonstrife
+ * @Last Modified time: 2022-04-26 14:49:01
 */
 
 /*
+Requires PHPfastCache (included in this plugin)
+Requires PHP 8.0+ because of the PHPfastCache library
+With caching disabled you may be able to run this plugin on lower versions of PHP, but this is untested.
 */
