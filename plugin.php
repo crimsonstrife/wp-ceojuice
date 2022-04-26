1bf328cc72c12af9e060557364b8cf5ff4aff597
Patrick Barnhardt <contact@patrickbarnhardt.info>
2022-04-25 23:25:26 -0400

    Create plugin.php

---
+++ plugin.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name:  CEOJuice API Connector
+Plugin URI:   https://patrickbarnhardt.com/plugins/ceojuice-api
+Description:  Worpress Plugin for contacting the CeoJuice API to retrieve and display NPS data and Testimonials. Based on the theme feature I created for
+Version:      1.0.0
+Author:       Patrick Barnhardt
+Author URI:   https://www.patrickbarnhardt.com
+License:      GPL3
+Text Domain:  ceojuice-api
+*/
+
+/*
+*/
